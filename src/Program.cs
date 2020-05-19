be9d9fe8f0642a67e73b9f9e8f2fae41c6374a92
csnewcs <cs.newcs0@gmail.com>
2020-05-19 20:04:02 +0900

    완성

--- src/Program.cs
+++ src/Program.cs
@@ -4,11 +4,11 @@ using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace src
+namespace InstallGTKOnWindows
 {
     class Program
     {
-        // msys2링크: http://repo.msys2.org/distrib/x86_64/msys2-x86_64-20190524.exe
+        // msys2링크: https://github.com/msys2/msys2-installer/releases/download/2020-05-17/msys2-x86_64-20200517.exe
         // 
         static void Main(string[] args)
         {
@@ -18,7 +18,7 @@ namespace src
             client.Proxy = null;
 
             Console.CursorVisible = false;
-            Console.WriteLine("MSYS2 다운로드중....");
+            Console.WriteLine("MSYS2 설치기 다운로드중....");
 
 
             bool doing = true;
@@ -41,34 +41,74 @@ namespace src
                 }
                 all = e.TotalBytesToReceive;
                 long now = e.BytesReceived;
-                decimal speed = now - last;
+                decimal speed = (now - last) * 2;
                 last = now;
                 
                 int remainder = (int)((all - last) / speed);
                 percentage = e.ProgressPercentage;
-                Console.Write($"{percentage}% ({pr.convertUnit(now)} / {pr.convertUnit(all)}), {pr.convertUnit(speed)}, {remainder / 60}분 {remainder % 60}초 남음      ");
+                Console.Write($"{percentage}% ({pr.convertUnit(now)} / {pr.convertUnit(all)}), {pr.convertUnit(speed)}/s, {remainder / 60}분 {remainder % 60}초 남음      ");
                 Console.SetCursorPosition(0, top);
                 };
-            client.DownloadFileAsync(new Uri("http://repo.msys2.org/distrib/x86_64/msys2-x86_64-20190524.exe"), "msys2install.exe");
+            client.DownloadFileAsync(new Uri("https://github.com/msys2/msys2-installer/releases/download/2020-05-17/msys2-x86_64-20200517.exe"), "msys2install.exe");
             while (doing)
             {
                 sec = false;
-                Thread.Sleep(1000);
+                Thread.Sleep(500);
             }
+            Console.WriteLine($"100% ({pr.convertUnit(all)} / {pr.convertUnit(all)}), 0 BYTE/s, 0분 0초 남음                   ");
+
+            Console.CursorVisible = true;
+            Thread.Sleep(50);
             
-            ProcessStartInfo startInfo = new ProcessStartInfo("msys2install.exe");
-            startInfo.WorkingDirectory = @"C:\";
-
-            Process msys2 = new Process();
-            msys2.StartInfo = startInfo;
-            msys2.Start();
-
+            Process.Start("msys2install.exe");
             Console.WriteLine("창이 뜨면 MSYS2를 설치해주세요");
-            msys2.Disposed += new Program().msys2Disposed;
-        }
-        void msys2Disposed(object sender, EventArgs e)
-        {
+            Process[] processArray = Process.GetProcessesByName("msys2install");
+            while (processArray.Length <= 0) 
+            {
+                processArray = Process.GetProcessesByName("msys2install");
+                Thread.Sleep(100);
+            }
+            GC.Collect();
+            while (processArray.Length > 0) 
+            {
+                processArray = Process.GetProcessesByName("msys2install");
+                Thread.Sleep(100);
+            }
+            GC.Collect();
             Console.WriteLine("MSYS2 설치 완료");
+
+            Process.Start("C:/msys64/msys2.exe", "pacman -Sy");
+            while (processArray.Length <= 0) 
+            {
+                processArray = Process.GetProcessesByName("mintty");
+                Thread.Sleep(100);
+            }
+            GC.Collect();
+            while (processArray.Length > 0) 
+            {
+                processArray = Process.GetProcessesByName("mintty");
+                Thread.Sleep(100);
+            }
+            GC.Collect();
+
+            Process.Start("C:/msys64/msys2.exe", "pacman -S mingw-w64-x86_64-gtk3 mingw-w64-x86_64-glade");
+            while (processArray.Length <= 0) 
+            {
+                processArray = Process.GetProcessesByName("mintty");
+                Thread.Sleep(100);
+            }
+            GC.Collect();
+            while (processArray.Length > 0) 
+            {
+                processArray = Process.GetProcessesByName("mintty");
+                Thread.Sleep(100);
+            }
+            GC.Collect();
+            Console.WriteLine("GTK 설치 완료");            
+            Environment.SetEnvironmentVariable("Path", Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.User) + ";C:\\msys64\\mingw64\\bin;C:\\msys32\\mingw32\\bin", EnvironmentVariableTarget.User);
+            Console.WriteLine("환경 변수 설정 완료\n설치를 끝내려면 아무 키나 누르세요...");
+            Console.ReadKey();
+
         }
         string convertUnit(decimal speed)
         {
